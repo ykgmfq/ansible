26f0bf8ec097688fc299fa05e09070c048a324b0
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2022-09-13 08:50:01 +0200

    Fix pg dump file group

--- roles/backup-base/files/pg_dump.fish
+++ roles/backup-base/files/pg_dump.fish
@@ -7,4 +7,5 @@ set file (printf "%s/pgdump_%s" $path $db)
 chmod u+w $file
 umask u=rw,go-rwx
 systemd-run --quiet --wait --collect --pipe --same-dir --service-type=oneshot --uid $uid /usr/bin/pg_dump --format=custom --file=$file $db
+chgrp postgres $file
 chmod u-wx,go-rwx $file

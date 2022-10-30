d04618b81f34e1f956051fb95883110ce1309c94
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2022-10-30 10:19:41 +0100

    Script tweaks

--- roles/cloud/files/container/src/install.sh
+++ roles/cloud/files/container/src/install.sh
@@ -5,27 +5,29 @@ function dnf { microdnf --{assumeyes,nodocs,setopt=install_weak_deps=0} $@; }
 echo "Using ID $1 for cloud user and group. Installing PHP version $2."
 echo "This is Fedora $fedora."
 cd /tmp
-# Repositories
+# Configure extra repositories
 curl --location --no-progress-meter --remote-name-all \
 https://rpms.remirepo.net/fedora/remi-release-$fedora.rpm \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$fedora.noarch.rpm
-rpm -i *rpm
+rpm -i *.rpm
 # Package management
 dnf module enable php:remi-$2
 dnf update
-dnf install systemd php php-{cli,bcmath,gmp,fpm,xml,process,gd,mbstring,intl,pecl-{apcu,imagick-im7},opcache,json,zip,pgsql,sodium} ImageMagick7-heic ffmpeg
+dnf install systemd ImageMagick7-heic ffmpeg \
+php{,-{cli,bcmath,gmp,fpm,xml,process,gd,mbstring,intl,opcache,json,zip,pgsql,sodium,pecl-{apcu,imagick-im7}}}
 dnf clean all
-# Permissions
-groupadd --gid $1 cloud
-useradd --system --uid $1 --gid $1 cloud
-# Config files
-mkdir --parents /etc/systemd/system/php-fpm.service.d
+# Create user and matching group
+echo u cloud $1 | systemd-sysusers -
+# Place PHP Config files
 rm /etc/php-fpm.d/*
 mv php/fpm.ini /etc/php-fpm.d/cloud.conf
 mv php/php.ini /etc/php.d/99-nextcloud.ini
+# Place system unit files
+mkdir --parents /etc/systemd/system/php-fpm.service.d
 mv units/override.conf /etc/systemd/system/php-fpm.service.d/
+new_units=$(ls units/)
 mv units/* /etc/systemd/system/
-rm -R *
-# System units
+rm -r *
+# Set system unit states
 systemctl disable systemd-oomd{,.socket} dbus{,.socket} systemd-userdbd.socket
-systemctl enable php-fpm cloud-{cron,upgrade}.timer
+systemctl enable php-fpm $new_units

1205d096bf0d25f0507a47d838f050d03fb67c13
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2023-10-30 14:18:21 +0100

    Rework cloud

--- roles/cloud/files/src/install.sh
+++ roles/cloud/files/src/install.sh
@@ -7,12 +7,11 @@ echo "This is Fedora $fedora."
 cd /tmp
 # Configure extra repositories
 f install \
-fedora-repos-modular \
 https://rpms.remirepo.net/fedora/remi-release-$fedora.rpm \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$fedora.noarch.rpm
-f module enable postgresql:14 php:remi-$2
+f module enable php:remi-$2
 # Package management
-f install systemd postgresql-server ffmpeg ImageMagick-heic \
+f install systemd ffmpeg ImageMagick-heic fcgi jq \
 php{,-{cli,bcmath,gmp,fpm,xml,process,gd,mbstring,intl,opcache,json,zip,pgsql,sodium,pecl-{apcu,imagick-im7}}}
 #f reinstall tzdata
 f clean all
@@ -23,11 +22,10 @@ rm /etc/php-fpm.d/*
 mv php/fpm.ini /etc/php-fpm.d/cloud.conf
 mv php/php.ini /etc/php.d/99-nextcloud.ini
 # Place system unit files
-mkdir --parents /etc/systemd/system/php-fpm.service.d
-mv units/override.conf /etc/systemd/system/php-fpm.service.d/
 new_units=$(ls units/)
 mv units/* /etc/systemd/system/
+mv alive.sh /usr/local/bin/
 rm -r *
 # Set system unit states
 systemctl disable $(ls /etc/systemd/system/multi-user.target.wants/) systemd-{userdb,login}d
-systemctl enable php-fpm postgresql $new_units
+systemctl enable php-fpm $new_units
--- roles/cloud/templates/build.fish
+++ roles/cloud/templates/build.fish
@@ -1,13 +1,14 @@
 #!/usr/bin/fish
-export (cat config.ini | xargs -L 1)
 set tag (basename (pwd))
-set uid (id --user $tag)
+set uid {{ users.cloud }}
+set fedora 38
+set php '8.1'
 echo "Base Image  | $fedora"
 echo "PHP version | $php"
-echo "Port        | $port"
 echo "Tag         | $tag"
 echo "User ID     | $uid"
 # Web server
+echo Web
 begin
     set ctr (buildah from --pull docker.io/library/caddy:2)
     buildah copy $ctr ./src/Caddyfile /etc/caddy/; or false
@@ -15,13 +16,11 @@ begin
     buildah commit --rm $ctr $tag-web
 end
 # PHP
+echo PHP
 begin
     set ctr (buildah from --pull quay.io/fedora/fedora:$fedora)
     buildah copy $ctr ./src tmp; or false
-    buildah config \
-        --port $port \
-        --cmd /sbin/init \
-        $ctr
-    buildah run $ctr /tmp/install.sh $uid $php; or false
+    buildah config --cmd /sbin/init $ctr
+    buildah run $ctr bash /tmp/install.sh $uid $php; or false
     buildah commit --rm $ctr $tag
 end

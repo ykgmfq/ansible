e6f1dea0e01ba73f6d119e77467d8aa7239bfa0b
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2023-10-30 16:54:36 +0100

    Ansible lint

--- ubuntu-bootstrap.fish
+++ ubuntu-bootstrap.fish
@@ -1,19 +1,14 @@
 #!/usr/bin/env fish
-function boot_strap -a host -d "Setup public key login for root and remove the default ubuntu user."
+function boot_strap -a host hostname -d "Setup public key login for root and remove the default ubuntu user."
     set pw (pwqgen)
     echo -e "Please set the new password to:\n$pw\nand exit the remote session."
     ssh ubuntu@$host
     if test $status = 0
         echo "Now starting ansible playbook."
-        ansible-playbook ubuntu-bootstrap.yml --extra-vars="ansible_password=$pw" --extra-vars="@secrets.yml" --vault-password-file=pw.txt -i $host,
+        ansible-playbook ubuntu-bootstrap.yml --extra-vars="ansible_password=$pw hostname=$hostname" --extra-vars="@secrets.yml" --vault-password-file=pw.txt -i $host,
     else
         echo "Non-clean exit from ssh"
         exit 1
     end
 end
-if test (count $argv) = 1:
-    boot_strap $argv[1]
-else
-    echo Usage: (status basename) host
-    exit 1
-end
+boot_strap $argv

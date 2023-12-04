277398ca0e8d096658a7f285402391ab6913b491
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2023-12-04 14:45:50 +0100

    Media cleanup

--- roles/media/files/install.sh
+++
@@ -1,7 +0,0 @@
-set -eu \
-&& fedora=$(rpm -E %fedora) \
-&& url="https://copr.fedorainfracloud.org/coprs/leo3418/jellyfin/repo/fedora-$fedora/leo3418-jellyfin-fedora-$fedora.repo" \
-&& curl --no-progress-meter --remote-name --output-dir /etc/yum.repos.d $url \
-&& dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$fedora.noarch.rpm \
-&& dnf -y install --setopt=install_weak_deps=0 jellyfin \
-&& dnf clean all

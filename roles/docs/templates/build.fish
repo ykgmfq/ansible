da1bb898df421314d2a822b6b021a0bcb40eb4fa
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-01-01 11:19:24 +0100

    Samba

--- roles/docs/templates/build.fish
+++ roles/docs/templates/build.fish
@@ -1,12 +1,11 @@
 #!/usr/bin/fish
 set tag (basename (pwd))
-set scan_id (id --user scan)
 set -x USERMAP_UID {{ users.docs }}
 set -x USERMAP_GID $USERMAP_UID
 set -x PAPERLESS_LOGGING_DIR /tmp/log
 echo User ID for scan: $USERMAP_UID
 set ctr (buildah from --pull ghcr.io/paperless-ngx/paperless-ngx:{{ versions.paperless }})
-and buildah run $ctr /bin/bash -c "set -e; groupadd --gid $scan_id scan && usermod -aG www-data,scan paperless && mkdir --mode='ugo=rwx' $PAPERLESS_LOGGING_DIR"
+and buildah run $ctr mkdir --mode='ugo=rwx' $PAPERLESS_LOGGING_DIR
 for e in USERMAP_{G,U}ID PAPERLESS_LOGGING_DIR
     and buildah config --env=$e $ctr
 end
---
+++ roles/samba/files/src/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+adduser --disabled-password --no-create-home --uid $idscan scan
+adduser --disabled-password --no-create-home --uid $idmedia media
+apk add --no-cache samba
+(echo $pwmedia; sleep 1; echo $pwmedia ) | smbpasswd -s -a media
+(echo $pwscan; sleep 1; echo $pwscan ) | smbpasswd -s -a scan

1e9263ec4b656e6174dcddba537ade72f787849f
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2024-02-23 16:46:01 +0100

    Icinga access to samba

--- roles/samba/files/src/install.sh
+++ roles/samba/files/src/install.sh
@@ -4,3 +4,5 @@ adduser --disabled-password --no-create-home --uid $idmedia media
 apk add --no-cache samba
 (echo $pwmedia; sleep 1; echo $pwmedia ) | smbpasswd -s -a media
 (echo $pwscan; sleep 1; echo $pwscan ) | smbpasswd -s -a scan
+mkdir /opt/monitor
+echo "OK" > /opt/monitor/m.txt
